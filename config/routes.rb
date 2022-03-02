58b780e847adbc56ace9d0489d1fcbe12a03c6f5
hirotaka_tsukada_desktop <hirotaka.tsukada.js@gmail.com>
2022-03-02 22:54:34 +0900

    2022.3.2

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
+  root 'pages#index'
+  get 'pages/index'
+  get 'pages/help'
+
+  resources :users
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+end
